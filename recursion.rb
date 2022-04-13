1f58b29467d84530cee3e9f962bc5ebe38957665
Tom Li <tomleslieli@yahoo.com>
2022-04-13 11:59:08 -0400

    finished exponent_2

--- recursion.rb
+++ recursion.rb
@@ -11,3 +11,27 @@ def range_iterative(min, max)
     sum
 end
 # p range_iterative(0, 3)
+
+def exponent_1(base, exp)
+
+    return 1 if exp == 0
+    base * exponent_1(base, exp - 1)
+
+end
+
+# p exponent_1(3, 5)
+
+def exponent_2(base, exp)
+
+    return 1 if exp == 0
+    return base if exp == 1
+
+    if exp % 2 == 0
+        exponent_2(base, exp / 2) ** 2
+    else
+        base * (exponent_2(base, (exp - 1) / 2) ** 2) 
+    end
+end
+
+# p exponent_2(2, 4)
+
